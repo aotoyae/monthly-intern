cc8742f6fd2bf154b77c3dec755c83ee30872e45
kimsohyeon <aotoyae@gmail.com>
2024-11-10 22:43:55 +0900

    feat: 로그인 폼

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,50 @@
+import { useState } from 'react';
+
 const Login = () => {
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+
+  // const handleLogin = async () => {
+  //   try {
+  //     const response = await axios.post(`${MOCK_URL}/login`);
+  //     console.log(response);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // handleLogin();
+  };
+
   return (
-    <div>
-      <h1>로그인</h1>
-    </div>
+    <main className="mt-44">
+      <div className="flex w-[420px] flex-col justify-center gap-12 rounded-lg border border-solid px-6 py-12">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-4">
+            <input
+              type="text"
+              value={id}
+              placeholder="아이디"
+              onChange={(e) => setId(e.target.value)}
+              className="pl-3 border border-solid rounded-sm h-14"
+            />
+            <input
+              type="password"
+              value={pw}
+              placeholder="비밀번호"
+              onChange={(e) => setPw(e.target.value)}
+              className="pl-3 border border-solid rounded-sm h-14"
+            />
+          </div>
+          <button className="h-12 font-bold tracking-wider text-white bg-red-500 rounded-sm">
+            로그인
+          </button>
+        </form>
+      </div>
+    </main>
   );
 };
 
